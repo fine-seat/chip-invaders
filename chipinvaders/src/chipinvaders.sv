d371404e9b36c6c6e8b8cd7f11b5c14dcfad69e9
Emil <emil.gschloessl@hm.edu>
2026-02-12 15:39:02 +0000

    setup basic data-structures for game-elements

--- chipinvaders/src/chipinvaders.sv
+++ chipinvaders/src/chipinvaders.sv
@@ -1,3 +1,17 @@
-module chipinvaders ();
-    
+module chipinvaders (
+    input clk,
+    input input_left,
+    input input_right,
+    input input_fire,
+);
+
+localparam alien_row_size = 5;
+localparam alien_column_size = 3;
+
+localparam coordinate_size = 10;
+localparam alive_size = 1;
+
+logic alien_array [0:alien_row_size*alien_column_size*(2*coordinate_size+alive_size)-1];
+logic cannon_array [0:2*coordinate_size+alive_size-1];
+  
 endmodule
