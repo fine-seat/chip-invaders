2a8be70ec3afb08c217c0cc4d798aea2d00e30eb
Benjamin Otto <git@otto.bayern>
2026-02-14 16:25:53 +0000

    Add default io

--- chipinvaders/src/chipinvaders.sv
+++ chipinvaders/src/chipinvaders.sv
@@ -1,3 +1,19 @@
-module chipinvaders ();
-    
+module chipinvaders (
+    input clk,
+    input rst_n,
+
+    // Buttons
+    input btn_d,
+    input btn_l,
+    input btn_r,
+    input btn_u,
+
+    // VGA
+    output [3:0] vga_r,
+    output [3:0] vga_g,
+    output [3:0] vga_b,
+    output vga_hs,
+    output vga_vs
+);
+
 endmodule
