39d717188ba6e2f51417f2b9c8b2281c7a55e8ab
Stefanie Schmid <schmid.stefanie@hm.edu>
2026-02-15 19:09:30 +0000

    adjust single alien logic

--- chipinvaders/test/test_alien.py
+++ chipinvaders/test/test_alien.py
@@ -23,25 +23,17 @@ class AlienTester:
         self.dut = dut
         self.clk = dut.clk
         self.rst_n = dut.rst_n
-        self.x_idx = dut.x_idx
-        self.y_idx = dut.y_idx
         self.hit = dut.hit
         self.dir = dut.dir
+        self.fire = dut.fire
         self.alive = dut.alive
         self.fired = dut.fired
-        self.x_pos = dut.x_pos
-        self.y_pos = dut.y_pos
-        
-    async def set_indices(self, x: int = 0, y: int = 0):
-        await RisingEdge(self.clk)
-        self.x_idx.value = x
-        self.y_idx.value = y
         
     async def reset_module(self):
         self.rst_n.value = 0
         await FallingEdge(self.clk)
         self.rst_n.value = 1
-        await FallingEdge(self.clk)
+        await RisingEdge(self.clk)
         
     async def set_hit(self, hit: int = 1):
         self.hit.value = hit
@@ -49,37 +41,32 @@ class AlienTester:
 
 @cocotb.test()
 async def test_init(dut):
-    """Test: Test inital values"""
-    # todo: edit when bitmaps are finished
-    alien_width = 10
-    alien_spacing = 10
-    
+    """Test: Test inital values"""    
     tester = AlienTester(dut)
 
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
-
-    for x in range(16):
-        for y in range(16):
-            expected_x_pos = (alien_width + alien_spacing) * x
-            expected_y_pos = (alien_width + alien_spacing) * y
-            
-            dut._log.info(f"Test {x+y}: Init alien on position {x}, {y}")
-            
-            await tester.set_indices(x, y)
     
-            await tester.reset_module()
-            
-            have_alive = tester.alive.value
-            have_fired = tester.fired.value
-            have_x_pos = int(str(tester.x_pos.value), 2)
-            have_y_pos = int(str(tester.y_pos.value), 2)
-            
-            assert have_x_pos == expected_x_pos, f"test_init (x_pos): Expected {expected_x_pos}, got {have_x_pos}"
-            assert have_y_pos == expected_y_pos, f"test_init (y_pos): Expected {expected_y_pos}, got {have_y_pos}"
-            assert have_fired == 0, f"test_init (fired): Expected 0, got {have_fired}"
-            assert have_alive == 1, f"test_init (alive): Expected 1, got {have_alive}"
+    # initial values
+    have_alive = tester.alive.value
+    have_fired = tester.fired.value
+    
+    assert have_fired == 0, f"test_init (fired): Expected 0, got {have_fired}"
+    assert have_alive == 1, f"test_init (alive): Expected 1, got {have_alive}"
 
+    # revert values
+    tester.alive.value = 0
+    tester.fired.value = 1
+
+    await tester.reset_module()
+    
+    # values after reset signal
+    have_alive = tester.alive.value
+    have_fired = tester.fired.value
+    
+    assert have_fired == 0, f"test_init (fired): Expected 0, got {have_fired}"
+    assert have_alive == 1, f"test_init (alive): Expected 1, got {have_alive}"
+    
     dut._log.info("✓ Init test passed")
     
 @cocotb.test()
