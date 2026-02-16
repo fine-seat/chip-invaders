b50be5078ed25743709b0461bcc6849b249d63a6
Stefanie Schmid <schmid.stefanie@hm.edu>
2026-02-16 17:13:59 +0000

    wip fix tests

--- chipinvaders/test/test_alien.py
+++ chipinvaders/test/test_alien.py
@@ -23,11 +23,13 @@ class AlienTester:
         self.dut = dut
         self.clk = dut.clk
         self.rst_n = dut.rst_n
-        self.hit = dut.hit
-        self.dir = dut.dir
-        self.fire = dut.fire
         self.alive = dut.alive
-        self.fired = dut.fired
+        self.movement_frequency = dut.movement_frequency
+        self.movement_direction = dut.movement_direction
+        self.armed = dut.armed
+        self.position_x = dut.position_x
+        self.position_y = dut.position_y
+        self.graphics = dut.graphics
         
     async def reset_module(self):
         self.rst_n.value = 0
@@ -35,56 +37,45 @@ class AlienTester:
         self.rst_n.value = 1
         await RisingEdge(self.clk)
         
-    async def set_hit(self, hit: int = 1):
-        self.hit.value = hit
-        await RisingEdge(self.clk)
+    # async def set_hit(self, hit: int = 1):
+    #     self.hit.value = hit
+    #     await RisingEdge(self.clk)
 
 @cocotb.test()
-async def test_init(dut):
-    """Test: Test inital values"""    
+async def test_reset(dut):
+    """Test: Test reset"""    
     tester = AlienTester(dut)
 
     clock = Clock(dut.clk, 10, unit="us")
     cocotb.start_soon(clock.start())
-    
-    # initial values
-    have_alive = tester.alive.value
-    have_fired = tester.fired.value
-    
-    assert have_fired == 0, f"test_init (fired): Expected 0, got {have_fired}"
-    assert have_alive == 1, f"test_init (alive): Expected 1, got {have_alive}"
-
-    # revert values
-    tester.alive.value = 0
-    tester.fired.value = 1
 
     await tester.reset_module()
     
-    # values after reset signal
-    have_alive = tester.alive.value
-    have_fired = tester.fired.value
+    have_x = int(str(tester.position_x.value), 2)
+    have_y = int(str(tester.position_y.value), 2)
     
-    assert have_fired == 0, f"test_init (fired): Expected 0, got {have_fired}"
-    assert have_alive == 1, f"test_init (alive): Expected 1, got {have_alive}"
+    # values after reset signal    
+    assert have_x == dut.INITIAL_POSITION_X.value, f"test_reset (x): Expected {dut.INITIAL_POSITION_X.value}, got {have_x}"
+    assert have_y == dut.INITIAL_POSITION_Y.value, f"test_reset (y): Expected {dut.INITIAL_POSITION_Y.value}, got {have_y}"
     
-    dut._log.info("✓ Init test passed")
+    dut._log.info("✓ Reset test passed")
     
-@cocotb.test()
-async def test_hit(dut):
-    """Test: Test hitting the alien"""
-    tester = AlienTester(dut)
+# @cocotb.test()
+# async def test_hit(dut):
+#     """Test: Test hitting the alien"""
+#     tester = AlienTester(dut)
     
-    clock = Clock(dut.clk, 10, unit="us")
-    cocotb.start_soon(clock.start())
+#     clock = Clock(dut.clk, 10, unit="us")
+#     cocotb.start_soon(clock.start())
        
-    await tester.set_hit()
+#     await tester.set_hit()
     
-    await RisingEdge(tester.clk)
-    have = tester.alive.value
+#     await RisingEdge(tester.clk)
+#     have = tester.alive.value
     
-    assert have == 0, f"test_hit: Expected 0, got {have}"
+#     assert have == 0, f"test_hit: Expected 0, got {have}"
     
-    dut._log.info("✓ Hitting test passed")
+#     dut._log.info("✓ Hitting test passed")
 
 
 def test_alien_runner():
@@ -101,6 +92,10 @@ def test_alien_runner():
         always=True,
         waves=True,
         timescale=("1ns", "1ps"),
+        parameters={
+            "INITIAL_POSITION_X": 0,
+            "INITIAL_POSITION_Y": 0
+        }
     )
 
     runner.test(hdl_toplevel="alien", test_module="test_alien", waves=True)
