c16738b59d8692a6c8350a67ff3b2ae112fe4c30
Stefanie Schmid <schmid.stefanie@hm.edu>
2026-02-16 16:34:39 +0000

    add verilog extension settings

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,3 +1,6 @@
 git submodule update --init --recursive
 
 git config --global --add safe.directory /workspaces/chip-invaders/chipinvaders/librelane
+
+nix profile add nixpkgs#verible
+apt install universal-ctags verilator
