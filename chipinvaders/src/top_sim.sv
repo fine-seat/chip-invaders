c420ec047363964eb47d7b2f4c33d57baa9b4353
Benjamin Otto <git@otto.bayern>
2026-02-20 10:54:58 +0000

    Adjust top_sim for colors

--- chipinvaders/src/top_sim.sv
+++ chipinvaders/src/top_sim.sv
@@ -6,12 +6,16 @@ module top_sim (
     input logic key_left,
     input logic key_right,
 
-    output logic [3:0] r,
+    output logic [1:0] r,
     g,
     b,
     output logic hsync,
     output logic vsync
 );
+
+  logic [3:0] vga_r;
+  logic [3:0] vga_g;
+  logic [3:0] vga_b;
 
   chipinvaders game (
       .clk(clk),
@@ -19,11 +23,15 @@ module top_sim (
       .btn_u(key_up),
       .btn_l(key_left),
       .btn_r(key_right),
-      .vga_r(r),
-      .vga_g(g),
-      .vga_b(b),
+      .vga_r(vga_r),
+      .vga_g(vga_g),
+      .vga_b(vga_b),
       .vga_hs(hsync),
       .vga_vs(vsync)
   );
+
+  assign r = vga_r[3:2];
+  assign g = vga_g[3:2];
+  assign b = vga_b[3:2];
 
 endmodule
