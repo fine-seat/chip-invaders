8f1a0609d5339031e1365fc24766b555204f52a3
Benjamin Otto <git@otto.bayern>
2026-03-27 07:38:17 +0000

    clean devcontainer config

--- .devcontainer/post-create.sh
+++ .devcontainer/post-create.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
-git config --global --add safe.directory '*'
 git submodule update --init --recursive
-export NIX_CONFIG="connect-timeout = 60
-stalled-download-timeout = 300"
+
+git config --global --add safe.directory /workspaces/chip-invaders/chipinvaders/librelane
+
 nix profile add nixpkgs#verible
 apt install -y universal-ctags verilator iverilog
